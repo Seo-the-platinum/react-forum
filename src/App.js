66b755a00ca4ba7a910679967f3c529a874f9798
Eliseo Flores <eliseo_flores530@yahoo.com>
2019-11-08 18:44:04 -0800

    added redux-thunk, corrected app.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Home from './components/Home'
+import CategoryView from './components/CategoryView'
+import PostView from './components/PostView'
+import AddorCreate from './components/AddorCreate'
+import AddComment from './components/AddComment'
+import EditPost from './components/EditPost'
+import EditComment from './components/EditComment'
+import handleInitialData from './actions/shared'
+import { connect } from 'react-redux'
+import styled, { css, createGlobalStyle } from 'styled-components'
+import NavBar from './components/NavBar'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const GlobalStyle= createGlobalStyle`
+  html, body {
+    height: 100%;
+    background: rgb(2, 150, 156);
+  }
+`
+
+class App extends Component {
+
+ state= {
+   loading: true,
+ }
+  componentDidMount() {
+    this.props.dispatch(handleInitialData())
+    .then(()=> {
+      this.setState((currState)=> ({
+        loading: false,
+      }))
+    })
+  }
+
+  render() {
+
+    return (
+
+      <Router>
+        <div>
+          <Route path='/' component={ NavBar } />
+          <Route exact path='/' component={ Home } />
+          <Route path='/categoryview/:path' component={ CategoryView } />
+          <Route path='/postview/:pid' component={ PostView } />
+          <Route path='/add/:pathname' component={ AddorCreate } />
+          <Route path='/addcomment/:pid' component={ AddComment } />
+          <Route path='/edit/:pid' component={ EditPost }/>
+          <Route path='/editcomment/:id' component={ EditComment } />
+          </div>
+      </Router>
+    )
+  }
 }
 
-export default App;
+
+export default connect()(App);
--- src/index.js
+++ src/index.js
@@ -3,8 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux'
+import reducer from './reducers'
+import middleware from './middleware'
+import { Provider } from 'react-redux'
+import styled, { css, createGlobalStyle } from 'styled-components'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const GlobalStyle= createGlobalStyle`
+  html, body {
+    min-height: 100%;
+    background: rgb(2, 150, 156);
+    padding: 0;
+    margin:0;
+  }
+`
+const store= createStore(reducer, middleware)
+
+ReactDOM.render(<><Provider store={store}><App /></Provider><GlobalStyle /></>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
