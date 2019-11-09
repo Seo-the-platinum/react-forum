cb7532b1d595aa500bb3d56ffbe4b43febbbf8c8
Eliseo Flores <eliseo_flores530@yahoo.com>
2019-11-08 18:59:02 -0800

    Updates

---
+++ precache-manifest.3d04671bd9060f5458c29197d237f11a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cd0a2aa496285b2655190cf6cacca1c",
+    "url": "/react-forum/index.html"
+  },
+  {
+    "revision": "e35126935617475cfcbe",
+    "url": "/react-forum/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "1e3efe558ddc58b9eeb1",
+    "url": "/react-forum/static/js/2.d9f4edb5.chunk.js"
+  },
+  {
+    "revision": "e35126935617475cfcbe",
+    "url": "/react-forum/static/js/main.fb96d200.chunk.js"
+  },
+  {
+    "revision": "562da3b238539c3486ee",
+    "url": "/react-forum/static/js/runtime-main.022af67f.js"
+  }
+]);
