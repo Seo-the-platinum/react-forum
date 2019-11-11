9dfa85e614d85419c7560a328c971649fd05690e
Eliseo Flores <eliseo_flores530@yahoo.com>
2019-11-11 11:17:24 -0800

    added basename again

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ class App extends Component {
   render() {
 
     return (
-      <Router>
+      <Router basename="/seo-the-platinum.github.io/react-forum/">
         <div>
           <Route path='/' component={ NavBar } />
           <Route exact path='/' component={ Home } />
