6ffa7b2f645911a02a5e0f7e7c1e4756ef138a73
Eliseo Flores <eliseo_flores530@yahoo.com>
2019-11-11 10:48:46 -0800

    changed HashRouter back to BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { HashRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './components/Home'
 import CategoryView from './components/CategoryView'
 import PostView from './components/PostView'
